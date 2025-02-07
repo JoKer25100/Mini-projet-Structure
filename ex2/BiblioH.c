768adc50b389b0a6bb542aa70420166dfae58b24
Eric <ericxia168@gmail.com>
2025-02-08 00:18:23 +0100

    debut exo2

---
+++ ex2/BiblioH.c
@@ -0,0 +1,4 @@
+#include "biblioH.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
---
+++ ex2/BiblioH.h
@@ -0,0 +1,15 @@
+#ifndef _BIBLIOLC_H_
+#define _BIBLIOLC_H_
+
+typedef struct livreh{
+    int clef;
+    struct livreh* suivant;
+} LivreH;
+
+typedef struct table{
+    int nE;
+    int m;
+    LivreH** T;
+}BiblioH;
+
+#endif
